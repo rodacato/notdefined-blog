8fb55b3d1e131d903297d898601b3df5c1e27ff9
Adrian Castillo <rodacato@gmail.com>
2025-02-26 13:42:51 -0600

    Add support for github flavored markdown
    [changelog]
    added: Add support for github flavored markdown
    
    <!-- ps-id: f6e7ac0d-1f8a-433c-8cdf-c2bc996a1392 -->

--- src/lib/markdownToHtml.ts
+++ src/lib/markdownToHtml.ts
@@ -2,23 +2,27 @@ import { unified } from 'unified'
 import rehypeStringify from 'rehype-stringify'
 import remarkParse from 'remark-parse'
 import remarkRehype from 'remark-rehype'
+import remarkGfm from 'remark-gfm'
 import { remarkMermaid } from './remark-mermaid'
 import rehypeHighlight from 'rehype-highlight'
 import rehypeHighlightCodeLines from 'rehype-highlight-code-lines'
 
 export default async function markdownToHtml (markdown: string) {
   const result = await unified()
-    .use(remarkParse) // Parse markdown string into markdown AST (Abstract Syntax Tree)
-    .use(remarkMermaid as any) // Convert Mermaid diagram code blocks into HTML divs
-    .use(remarkRehype, { // Transform markdown AST into HTML AST
+    .use(remarkParse)                 // Parse markdown string into markdown AST (Abstract Syntax Tree)
+    .use(remarkGfm)                   // Add GFM (GitHub Flavored Markdown) support
+    .use(remarkMermaid)               // Convert Mermaid diagram code blocks into HTML divs
+    .use(remarkRehype, {              // Transform markdown AST into HTML AST
       allowDangerousHtml: true
     })
-    .use(rehypeHighlight) // Add syntax highlighting to code blocks
-    .use(rehypeHighlightCodeLines, { // Add line numbers to highlighted code blocks
+    .use(rehypeHighlight)             // Add syntax highlighting to code blocks
+    .use(rehypeHighlightCodeLines, {  // Add line numbers to highlighted code blocks
       showLineNumbers: true,
       lineContainerTagName: 'div'
     })
-    .use(rehypeStringify, { allowDangerousHtml: true }) // Convert final HTML AST back to string
+    .use(rehypeStringify, {           // Convert final HTML AST back to string
+      allowDangerousHtml: true
+    })
     .process(markdown)
 
   return result.toString()
